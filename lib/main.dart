1161e8eca32caa0143d075c53596f3751b95f14c
Waseem Ashraf <waseemashraf47@gmail.com>
2024-01-08 17:20:27 +0500

    Login screen has been implemented 40m

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/firebase_options.dart';
-import 'package:instagram_clone/responisve/mobile_screen_layout.dart';
-import 'package:instagram_clone/responisve/responsive_layout_screen.dart';
-import 'package:instagram_clone/responisve/web_screen_layout.dart';
+// import 'package:instagram_clone/responisve/mobile_screen_layout.dart';
+// import 'package:instagram_clone/responisve/responsive_layout_screen.dart';
+// import 'package:instagram_clone/responisve/web_screen_layout.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 Future<void> main() async {
@@ -26,9 +27,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(
-          webScreenLayout: WebScreenLayout(),
-          mobileScreenLayout: MobileScreenLayout()),
+    //   home: const ResponsiveLayout(
+    //       webScreenLayout: WebScreenLayout(),
+    //       mobileScreenLayout: MobileScreenLayout()),
+    // );
+    home: const LoginScreen(),
     );
   }
 }
