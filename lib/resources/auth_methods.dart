b6a8e3dfa3cf623225470b4f130a6f3f6170f384
Waseem Ashraf <waseemashraf47@gmail.com>
2024-01-19 12:21:31 +0500

    Image storage has been done 1.44h

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,54 +1,57 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:instagram_clone/resources/storage_methods.dart';
 // import 'package:flutter/material.dart';
 
-class AuthMethod{
-  final FirebaseAuth _auth= FirebaseAuth.instance;
+class AuthMethod {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future <String> signUpUser({
+  Future<String> signUpUser({
     required String email,
     required String password,
     required String username,
     required bio,
-    // required Uint8List file,
+    required Uint8List file,
+  }) async {
+    String res = "Some error occured";
 
-  })
-  async {String res = "Some error occured";
-
-  try{
-    if (email.isNotEmpty || password.isNotEmpty || username.isNotEmpty || bio.isNotEmpty /*|| file != null*/
-    ){
-      //register user
-      UserCredential cred= await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      print(cred.user!.uid);
-      await _firestore.collection('users').doc(cred.user!.uid).set({ 
-        'username': username,
-        'uid' : cred.user!.uid,
-        'email': email,
-        'bio': bio,
-        'followers': [],
-        'following': [],
-      });
-      // await _firestore.collection('user').add({
-      //   'username': username,
-      //   'uid' : cred.user!.uid,
-      //   'email': email,
-      //   'bio': bio,
-      //   'followers': [],
-      //   'following': [],
-      // });
-      res = 'success';
-      // TODO: add user to our database
+    try {
+      if (email.isNotEmpty ||
+              password.isNotEmpty ||
+              username.isNotEmpty ||
+              bio.isNotEmpty /*|| file != null*/
+          ) {
+        //register user
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        print(cred.user!.uid);
+        String photoUrl = await StorageMethods()
+            .upLoadImageToStorage('profilePics', file, false);
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': [],
+          'photoUrl': photoUrl,
+        });
+        // await _firestore.collection('user').add({
+        //   'username': username,
+        //   'uid' : cred.user!.uid,
+        //   'email': email,
+        //   'bio': bio,
+        //   'followers': [],
+        //   'following': [],
+        // });
+        res = 'success';
+        // TODO: add user to our database
+      }
+    } catch (err) {
+      res = err.toString();
     }
-
-  }
-  catch(err){
-    res = err.toString();
-  }
     return res;
-}
+  }
 }
